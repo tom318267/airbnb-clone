e725a82a2fc6e660a4c6647c0ceffdc61528e6ac
Thomas Rago <thomasrago@Thomass-Air.fios-router.home>
2021-08-16 17:28:04 -0400

    add mapbox

---
+++ components/Map.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import getCenter from "geolib/es/getCenter";
+
+const Map = ({ searchResults }) => {
+  const [selectedLocation, setSelectedLocation] = useState({});
+
+  const coordinates = searchResults.map((result) => ({
+    longitude: result.long,
+    latitude: result.lat,
+  }));
+
+  const center = getCenter(coordinates);
+
+  const [viewPort, setViewPort] = useState({
+    width: "100%",
+    height: "100%",
+    latitude: center.latitude,
+    longitude: center.longitude,
+    zoom: 11,
+  });
+
+  return (
+    <ReactMapGL
+      mapStyle="mapbox://styles/tommy318267/cksevp6b821d817n0xdr0j7jh"
+      mapboxApiAccessToken={process.env.mapbox_key}
+      {...viewPort}
+      onViewportChange={(nextViewport) => setViewPort(nextViewport)}
+    >
+      {searchResults.map((result) => (
+        <div key={result.long}>
+          <Marker
+            longitude={result.long}
+            latitude={result.lat}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <p
+              role="img"
+              onClick={() => setSelectedLocation(result)}
+              className="cursor-pointer text-2xl animate-bounce"
+              aria-label="push-pin"
+            >
+              📌
+            </p>
+          </Marker>
+          {/* Pop up should show if we click on marker */}
+          {selectedLocation.long === result.long ? (
+            <Popup
+              onClose={() => setSelectedLocation({})}
+              closeOnClick={true}
+              latitude={result.lat}
+              longitude={result.long}
+            >
+              {result.title}
+            </Popup>
+          ) : (
+            false
+          )}
+        </div>
+      ))}
+    </ReactMapGL>
+  );
+};
+
+export default Map;
--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-    images: {
-        domains: ['links.papareact.com']
-    }
-}
+  images: {
+    domains: ["links.papareact.com"],
+  },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoidG9tbXkzMTgyNjciLCJhIjoiY2tzZXZiZGxjMTRmMTJ1bnJtZ2x2cmdqMCJ9.8V9OZ_GoFfPoAxgPHKmBVg",
+  },
+};
