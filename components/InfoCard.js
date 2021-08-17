b97434f9485058c708c11df52c542fddce514db6
Thomas Rago <thomasrago@Thomass-Air.fios-router.home>
2021-08-16 21:46:18 -0400

    adjust infocard

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -16,32 +16,29 @@ const InfoCard = ({
     <div className="flex py-7 px-2 border-b cursor-pointer hover:opacity-80 hover:shadow-lg transition duration-200 ease-out first:border-t">
       <div className="relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0">
         <Image
-          className="rounded-2xl"
           src={img}
-          layout="fill"
           objectFit="cover"
+          layout="fill"
+          className="rounded-2xl"
         />
       </div>
-
       <div className="flex flex-col flex-grow pl-5">
         <div className="flex justify-between">
-          <p>{location}</p>
+          <p className="text-gray-500 text-sm">{location}</p>
           <HeartIcon className="h-7 cursor-pointer" />
         </div>
-
         <h4 className="text-xl">{title}</h4>
-
         <div className="border-b w-10 pt-2" />
         <p className="pt-2 text-sm text-gray-500 flex-grow">{description}</p>
 
-        <div className="flex justify-between items-end pt-5">
+        <div className="flex items-end justify-between pt-5">
           <p className="flex items-center">
             <StarIcon className="h-5 text-red-400" />
             {star}
           </p>
 
           <div>
-            <p className="text-lg font-semibold pb-2 lg:text-2xl">{price}</p>
+            <p className="text-lg lg:text-2xl font-semibold pb-2">{price}</p>
             <p className="text-right font-extralight">{total}</p>
           </div>
         </div>
